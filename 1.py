657832f9b3f23e2f1334161174d716d3ca455cd2
acer <acer@boss>
2018-04-07 12:34:51 +0530

    add files

---
+++ 1.py
@@ -0,0 +1,7 @@
+from urllib.parse import urlparse, parse_qs
+address = 'https://www.google.com/search?q=gray+squirrel&tbm=isch'
+parts = urlparse(address)
+print(parts)
+print(parts.query)
+query = parse_qs(parts.query)
+print(query)
